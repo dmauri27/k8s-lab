a4b903552474d9dfb06b045ddcde838e0ca5c1fa
Diego Mauri <diegomauri@bluewin.ch>
2019-09-20 16:25:52 +0200

    Vagrant file and k8s cluster install script

---
+++ Vagrantfile
@@ -0,0 +1,42 @@
+Vagrant.configure("2") do |config|
+  config.vm.synced_folder '.', '/vagrant', disabled: true
+  config.ssh.forward_agent = true
+  config.ssh.forward_x11 = true
+
+  config.vm.define "master-01" do |w1|
+   w1.vm.box = "bento/ubuntu-18.04"
+   w1.vm.network "private_network", ip: "192.100.50.200",
+     virtualbox__intnet: true
+   w1.vm.hostname = "master"
+   w1.vm.provision :shell, path: "script.sh"
+   w1.vm.provider "virtualbox" do |w1|
+     w1.memory = 3072
+     w1.cpus = 2
+   end
+  end
+
+  config.vm.define "worker-01" do |w1|
+   w1.vm.box = "bento/ubuntu-18.04"
+   w1.vm.network "private_network", ip: "192.100.50.201",
+     virtualbox__intnet: true
+   w1.vm.hostname = "worker-01"
+   w1.vm.provision :shell, path: "script.sh"
+   w1.vm.provider "virtualbox" do |w1|
+     w1.memory = 2048
+     w1.cpus = 2
+   end
+  end
+
+  config.vm.define "worker-02" do |w1|
+   w1.vm.box = "bento/ubuntu-18.04"
+   w1.vm.network "private_network", ip: "192.100.50.202",
+     virtualbox__intnet: true
+   w1.vm.hostname = "worker-02"
+   w1.vm.provision :shell, path: "script.sh"
+   w1.vm.provider "virtualbox" do |w1|
+     w1.memory = 2048
+     w1.cpus = 2
+   end
+  end
+
+end
---
+++ script.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Disable swap
+sudo swapoff -a
+
+# Install Docker
+sudo apt-get update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install -y \
+	docker-ce=5:18.09.0~3-0~ubuntu-bionic \
+	docker-ce-cli=5:18.09.0~3-0~ubuntu-bionic containerd.io
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart docker.
+systemctl daemon-reload
+systemctl restart docker
+
+# Install kubectl, kubeadm
+sudo apt-get update && sudo apt-get install -y sudo apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+if [ "$HOSTNAME" = "master" ]; then
+  kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address `ip addr show eth1 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
+  mkdir -p "$HOME/.kube"
+  sudo cp -i /etc/kubernetes/admin.conf "$HOME/.kube/config"
+  sudo chown $(id -u):$(id -g) "$HOME/.kube/config"
+  kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/calico.yaml
+fi
